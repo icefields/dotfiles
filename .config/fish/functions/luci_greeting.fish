b5da9b1980ffecfe959631c4e58ff8bf7c71889e
lucifer <lucifer@arch.lucifer-Predator-PH317-52>
2023-12-30 12:52:00 -0500

    fixed luci greeting

--- .config/fish/functions/luci_greeting.fish
+++ .config/fish/functions/luci_greeting.fish
@@ -1,9 +1,9 @@
 function luci_greeting
-      echo " _                   _ " | lolcat
-    	echo "| |                 (_)" | lolcat 
-    	echo "| |     _   _   ___  _ " | lolcat
-    	echo "| |    | | | | / __|| |" | lolcat
-    	echo "| |____| |_| || (__ | |" | lolcat
-    	echo "\_____/ \__,_| \___||_|" | lolcat
+      	echo "       _                   _ " | lolcat
+    	echo "      | |                 (_)" | lolcat 
+    	echo "      | |     _   _   ___  _ " | lolcat
+    	echo "      | |    | | | | / __|| |" | lolcat
+    	echo "      | |____| |_| || (__ | |" | lolcat
+    	echo "      \_____/ \__,_| \___||_|" | lolcat
     	echo " "
- end
+end
