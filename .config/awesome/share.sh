d3bc52ea842dddeb71dfac8aa1447ce617dcceab
icefields <arctic.icefields@slmails.com>
2024-05-25 12:23:12 -0400

    added external colour lua script, added share dmenu script

--- .config/awesome/share.sh
+++ .config/awesome/share.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-file=$(find -L $HOME \( -path $HOME/.wine -o -path $HOME/.steam -o -path $HOME/Code -o -path '*/.*' \) -prune -o -type f \( -iname "*.png" -o -iname "*.jpg" -o -iname "*.mpg" -o -iname "*.mpeg" -o -iname "*.webp" -o -iname "*.pdf" -o -iname "*.jpeg" -o -iname "*.zip" -o -iname "*.txt" \) | dmenu -i -l 25)
+file=$(find -L $HOME \( -path $HOME/.wine -o -path $HOME/.steam -o -path $HOME/Code -o -path '*/.*' \) -prune -o -type f \( -iname "*.png" -o -iname "*.jpg" -o -iname "*.mpg" -o -iname "*.mpeg" -o -iname "*.webp" -o -iname "*.pdf" -o -iname "*.jpeg" -o -iname "*.zip" -o -iname "*.txt" -o -iname "*.mp3" -o -iname "*.flac" -o -iname "*.wav" -o -iname "*.m4a" \) | dmenu -i -l 25)
 curl -F"file=@$file" 0x0.st | xclip -selection c
 notify-send "Link Copied"
