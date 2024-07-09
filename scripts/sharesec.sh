7e017f00fe93c9841e2f7d8adcdb995a6a833288
Antonio <149625124+icefields@users.noreply.github.com>
2024-07-09 20:34:44 +0000

    Update sharesec.sh

--- scripts/sharesec.sh
+++ scripts/sharesec.sh
@@ -2,9 +2,8 @@
 
 dir=$(uuidgen | cut -d'-' -f1)
 mkdir /tmp/$dir
-cp $(fzf -m) /tmp/$dir
+cp $(find ~ -type f | fzf -m) /tmp/$dir
 zip -r /tmp/$dir.zip /tmp/$dir
 zipcloak /tmp/$dir.zip
 curl -F"file=@/tmp/$dir.zip" 0x0.st | xclip -selection c
 notify-send "Link Copied"
-
