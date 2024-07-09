a8ca223a52773d1bc54547671a86e83633f395d6
Antonio <149625124+icefields@users.noreply.github.com>
2024-07-09 20:38:15 +0000

    Update sharesec.sh

--- scripts/sharesec.sh
+++ scripts/sharesec.sh
@@ -2,7 +2,15 @@
 
 dir=$(uuidgen | cut -d'-' -f1)
 mkdir /tmp/$dir
-cp $(find ~ -type f | fzf -m) /tmp/$dir
+
+# if arguments are provided use them, otherwise do a manual selection
+if [ -z "$1" ]
+  then
+    cp $(find ~ -type f | fzf -m) /tmp/$dir
+  else
+    cp $1 /tmp/$dir
+fi
+
 zip -r /tmp/$dir.zip /tmp/$dir
 zipcloak /tmp/$dir.zip
 curl -F"file=@/tmp/$dir.zip" 0x0.st | xclip -selection c
