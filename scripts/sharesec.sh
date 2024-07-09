265cdacd9c8f1ccf8a3b2a16544424ca2b8ff2fd
Antonio <149625124+icefields@users.noreply.github.com>
2024-07-09 20:22:16 +0000

    Update sharesec.sh

--- scripts/sharesec.sh
+++ scripts/sharesec.sh
@@ -2,7 +2,7 @@
 
 dir=$(uuidgen | cut -d'-' -f1)
 mkdir /tmp/$dir
-cp $1 /tmp/$dir
+cp $(fzf -m) /tmp/$dir
 zip -r /tmp/$dir.zip /tmp/$dir
 zipcloak /tmp/$dir.zip
 curl -F"file=@/tmp/$dir.zip" 0x0.st | xclip -selection c
