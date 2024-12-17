ea1d677f31ede6ebffdff47b870ae5eeb9e28b61
icefields <arctic.icefields@slmails.com>
2024-12-16 23:42:11 -0500

    autostart

--- .config/awesome/autostart.sh
+++ .config/awesome/autostart.sh
@@ -8,8 +8,8 @@ run() {
 }
 
 # run picom
-run "/home/lucifer/.config/awesome/screens.sh"
-run "/home/lucifer/.config/awesome/picom_delayed.sh"
+run "$HOME/.config/awesome/screens.sh"
+run "$HOME/.config/awesome/picom_delayed.sh"
 run lxqt-policykit-agent
 run blueman-applet
 run nm-applet
@@ -17,7 +17,9 @@ run redshift-gtk
 run xfce4-clipman
 run xfce4-power-manager
 run keepassxc
-run "/home/lucifer/.config/awesome/start_filemanager.sh"
-run "/home/lucifer/apps/Joplin/Joplin-2.12.18.AppImage"
-run "/home/lucifer/.config/awesome/start_nextcloud.sh"
-run "/home/lucifer/.config/awesome/start_tuta.sh"
+run "$HOME/.config/awesome/start_filemanager.sh"
+run "$HOME/apps/Joplin/Joplin-2.12.18.AppImage"
+run "$HOME/.config/awesome/start_nextcloud.sh"
+run "$HOME/.config/awesome/start_tuta.sh"
+run "$HOME/.config/awesome/autostart-custom.sh"
+
