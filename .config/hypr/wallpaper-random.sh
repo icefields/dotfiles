2116be9bb056de2ce127a2ccb381eafa850ffe26
icefields <arctic.icefields@slmails.com>
2024-12-19 19:10:44 -0500

    hyprland fixed random wallpaper script

--- .config/hypr/wallpaper-random.sh
+++ .config/hypr/wallpaper-random.sh
@@ -4,16 +4,19 @@
 DIRECTORY="$HOME/Pictures/wallpapers/current"
 cd "$DIRECTORY" || { echo "Directory not found"; exit 1; }
 
+# start log file
+date > log-wallpapers.txt
+
 # Check if the directory is empty
 if [ -z "$(ls -A "$DIRECTORY")" ]; then
-  echo "No files found in the directory"
+  echo "No files found in the directory" >> log-wallpapers.txt
   exit 1
 fi
 
 # Pick 3 random wallpapers
-FILE0=$(ls | shuf -n 1)
-FILE1=$(ls | shuf -n 1)
-FILE2=$(ls | shuf -n 1)
+FILE0=$(ls *.png | shuf -n 1)
+FILE1=$(ls *.png | shuf -n 1)
+FILE2=$(ls *.png | shuf -n 1)
 
 # Rename the chosen files to wall0.png, wall1.png and wall2.png
 # corresponding links to those 3 files have to be present in
@@ -26,3 +29,5 @@ FILE2=$(ls | shuf -n 1)
 cp "$FILE0" "wall0.png"
 cp "$FILE1" "wall1.png"
 cp "$FILE2" "wall2.png"
+
+echo "$FILE0 $FILE1 $FILE2" >> log-wallpapers.txt
