07ae690bb935c3c6c9aa0692904ade904467a1c0
icefields <arctic.icefields@slmails.com>
2026-02-01 18:46:32 -0500

    cleanup

--- .config/awesome/currency/api_config.lua
+++ .config/awesome/currency/api_config.lua
@@ -1,6 +1,6 @@
 -- === CONFIG ===
 local config = { }
-config.baseUrl = HomeEnv.CURRENCY_API --"http://192.168.50.161:36661/get?coin=CAD"
+config.baseUrl = HomeEnv.CURRENCY_API
 config.symbols = { "󰆬", "󰞺", "󰞻", "󰆭", "󰆮", "󰞼", "󰇁" }
 config.cacheDir = os.getenv("HOME") .. "/.cache"
 config.filePath = config.cacheDir .. "/awesome-currency.json"
