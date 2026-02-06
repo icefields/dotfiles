f5b47ef20b14d52a31363498c8743bd98e298292
icefields <arctic.icefields@slmails.com>
2026-02-05 21:56:49 -0500

    Todo Widget, Awesome, nextcloud_config_example

--- .config/awesome/todo-widget/nextcloud_config_example.lua
+++ .config/awesome/todo-widget/nextcloud_config_example.lua
@@ -2,7 +2,10 @@
 local config = { }
 config.username = "nextcloud_username"
 config.appPassword = "app_password"
-config.baseUrl = "https://your.nextcloud.url/remote.php/dav/calendars/".. config.username .. "/tasklist_name/"
+config.baseUrl = "https://your.nextcloud.url/remote.php/dav/calendars/".. config.username
+
+-- tasklists to pull from Nextcloud
+config.tasklists = { "personal", "shopping", "work" }
 
 -- do not edit the next 2 lines
 config.cacheDir = os.getenv("HOME") .. "/.cache"
